c19e33ab075a77ac377000ceed9866db6714235d
prajaktaarunborkar <30730983+prajaktaarunborkar@users.noreply.github.com>
2017-08-17 00:05:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,9 @@ app.get('/', function (req, res) {
 app.get('/article-one', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
-
+app.get('/article-two', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
